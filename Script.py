54afd5c3b4a54f8f56d761f32962c9cc13d33e05
batloltoman <93631837+batloltoman@users.noreply.github.com>
2021-11-13 13:50:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -30,7 +30,7 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Bot Supports both url and alert inline buttons.
+- This Bot Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
