b21f00f93a6084fdb84b43d217682336df1a635e
batloltoman <93631837+batloltoman@users.noreply.github.com>
2021-11-14 15:08:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
  
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Team Renegades
 fi
-cd /EvaMaria
+cd/Team Renegades
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
