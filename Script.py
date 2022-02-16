157e9d75615ffc8027085151559c7493cdfd7092
batloltoman <93631837+batloltoman@users.noreply.github.com>
2022-02-16 20:00:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,12 +10,12 @@ class script(object):
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+    SOURCE_TXT = """<b>⚡About Me⚡</b>
+❤I Love Nezuko Chan so much🥺
+⚡I use thunder breathing but I am too scared😰
 
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+<b>Don't Ever Give Up</b>
+- <a href=https://t.me/cp_renegade>🌈Team Renegades</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
