83ac1c484ccba8d77d515295e2111c4fc9606623
batloltoman <93631837+batloltoman@users.noreply.github.com>
2022-02-18 13:58:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -76,7 +76,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("Don't Do It Again", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -677,7 +677,7 @@ async def auto_filter(client, msg, spoll=False):
             url = imdb['url']
         )
     else:
-        cap = f"Hey I have found  {kichu_ekta} what i found for your query {search}"
+        cap = f"Hey I have found <code>{current}</code> what i found for your query {search}"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
@@ -741,5 +741,5 @@ async def advantage_spell_chok(msg):
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?", reply_markup=InlineKeyboardMarkup(btn))
     
-    kichu_ekta = len(files)
+    kichu_ekta = len(file)
 
