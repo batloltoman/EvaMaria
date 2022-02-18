3bdc36eadb3d8ceb1e72e2ad6152b01e56f90250
batloltoman <93631837+batloltoman@users.noreply.github.com>
2022-02-18 12:39:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -149,9 +149,9 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("Do your work
+        return await query.answer("Do your work", show_alert=true)
 
-Don't waste your time here", show_alert=True)
+
     if movie_  == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
