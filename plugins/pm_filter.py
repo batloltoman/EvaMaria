43763d534b07b24058b5d80842f32f6d8a6e70d9
batloltoman <93631837+batloltoman@users.noreply.github.com>
2022-02-18 14:11:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -677,7 +677,7 @@ async def auto_filter(client, msg, spoll=False):
             url = imdb['url']
         )
     else:
-        cap = f"_⚡Hey, I have found this on ur query⚡_**{search}**\n_Join @cp_renegade to know more about us✨_"
+        cap = f"<i>⚡Hey, I have found this on ur query⚡</i>**{search}**\n<i>Join @cp_renegade to know more about us✨</i>"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
